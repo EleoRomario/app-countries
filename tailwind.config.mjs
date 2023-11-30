04af562ed3d08632ec2b21a2e5076b47b4feefdc
EleoRomario <eleo.romario@gmail.com>
2023-11-30 17:27:56 -0500

    Add React and Tailwind integrations

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#3361ff",
+				ligth: "#fafbfc",
+				title: {
+					DEFAULT: "#192233",
+					200: "#7d8fb2",
+					500: "#5c7099",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
